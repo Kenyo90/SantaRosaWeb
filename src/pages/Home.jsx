acd689cb2a201695db1f5183cde8135fba888026
Kenyo Saenz Rojas <saenzkenyo@gmail.com>
2026-03-04 17:56:10 -0500

    Branch Develop

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Box } from '@chakra-ui/react'
+
+
+const Home = () => {
+  return (
+    <div>
+      <Box colorScheme="teal" size="lg">
+        Branch Developer 🚀 
+      </Box>
+    </div>
+  )
+}
+
+export default Home
