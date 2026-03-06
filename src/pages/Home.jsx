6d4140be9cea3db89e234cfd732d66a55745596c
Kenyo Saenz Rojas <saenzkenyo@gmail.com>
2026-03-05 23:38:30 -0500

    (feature) cracion del navegador

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import { Box } from '@chakra-ui/react'
-
+import React from "react";
+import { Box, Flex, HStack, VStack, InputGroup, Input, IconButton } from "@chakra-ui/react";
+import NavBar from "../components/NavBar";
 
 const Home = () => {
-  return (
-    <div>
-      <Box colorScheme="teal" size="lg">
-        Hola Chakra 🚀
-      </Box>
-    </div>
-  )
-}
 
-export default Home
+  return (
+    <Box w='100vw'>
+        <NavBar/>
+    </Box>
+  
+  );
+};
+
+export default Home;
